3a0f77a7f27e88450dd4b5b9a87e0e5dfe87421e
Ramadan <ramadan@example.com>
2026-07-26 20:33:21 +0300

    updated

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ connectedDB();
 app.get("/", (req, res) => {
   return res.status(400).json({
     status_code: 200,
-    message: "WELCOME",
-    data: null,
+    message: "WELCOME To back end",
+    data: [],
   });
 });
 
